4e0647db728047f4f017649c92b27933af45a2b1
Rony Sheik <rony.sheik@hexagon.com>
2024-12-19 10:18:46 -0800

    updated style app and added favicon

--- src/views/RelatedResources.tsx
+++ src/views/RelatedResources.tsx
@@ -2,12 +2,22 @@ import React from "react";
 
 
 const RelatedResources = () => {
+    const linkText: string = "Awqat.net";
+    const linkUrl: string = "https://awqat.net/";
+
     return (
-        <a  style={{display:'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '15px'}}
-         href="https://awqat.net/" >
-             Awqat.net
-        </a>
-    )
-}
+        <div style={{ textAlign: 'center', paddingTop: '15px' }}>
+            <p>Find Lower Mainland Masjid prayer timings below</p>
+            <a 
+                href={linkUrl} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                style={{ textDecoration: 'none', color: '#007BFF' }}
+            >
+                {linkText}
+            </a>
+        </div>
+    );
+};
 
 export default RelatedResources;
